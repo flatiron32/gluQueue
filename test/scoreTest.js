bec1da18900d5f91a0bbc331fca8e04757e0b9ba
Jacob Tomaw <jacob.tomaw@orbitz.com>
2014-12-10 22:57:42 -0600

    Setup database port for travis-ci

--- test/scoreTest.js
+++ test/scoreTest.js
@@ -1,11 +1,7 @@
 /*jshint expr: true*/
 var should = require("should");
 var mysql = require('mysql');
-var nconf = require('nconf');
-
-nconf.argv().env().file({
-  file: 'db/config.json'
-});
+var ds = require('../lib/datasource.js');
 
 var Score = require('../lib/score.js').Score;
 
@@ -15,13 +11,7 @@ describe("saveScore", function() {
       var repo = "repo 1";
       var score = new Score(repo);
       score.score = 100;
-
-      var connection = mysql.createConnection({
-        host: nconf.get('database:host'),
-        port: nconf.get('database:port'),
-        user: nconf.get('database:user'),
-        database: 'score'
-      });
+      var connection = mysql.createConnection(ds.get('score'));
 
       var initialCount = 0;
       connection.query('SELECT count(*) as count FROM scores', function(err, rows, fields) {
@@ -31,12 +21,7 @@ describe("saveScore", function() {
       connection.end();
       score.save(function(err) {
         (!err).should.be.true;
-        var connection = mysql.createConnection({
-          host: nconf.get('database:host'),
-          port: nconf.get('database:port'),
-          user: nconf.get('database:user'),
-          database: 'score'
-        });
+        var connection = mysql.createConnection(ds.get('score'));
 
         connection.query('SELECT count(*) as count FROM scores', function(err, rows, fields) {
           (!err).should.be.true;
